f830bb91ec1a22bfb109d530edcff55173f1c572
ShiningRush <xuxin@followme-inc.com>
2018-09-17 19:47:28 +0800

    upload out png

--- example/main.go
+++ example/main.go
@@ -18,11 +18,11 @@ func main() {
 
 	// init avatarbuilder, you need to tell builder ttf file and how to alignment text
 	ab := avatarbuilder.NewAvatarBuilder("./SourceHanSansSC-Medium.ttf", &calc.SourceHansSansSCMedium{})
-	ab.SetBackgroundColorHex(colors[1])
+	ab.SetBackgroundColorHex(colors[3])
 	ab.SetFrontgroundColor(color.White)
 	ab.SetFontSize(80)
 	ab.SetAvatarSize(200, 200)
-	if err := ab.GenerateImageAndSave("GE", "./outEg.png"); err != nil {
+	if err := ab.GenerateImageAndSave("缄默", "./outCn.png"); err != nil {
 		fmt.Println(err)
 		return
 	}
